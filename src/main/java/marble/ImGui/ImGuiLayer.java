8020ad8f9325ea9c27d8d5abf7235b844c22b435
Christian Kjær <chrkj@itu.dk>
2022-01-11 23:31:04 +0100

    Feature: Vsync checkbox to diagnostics

--- src/main/java/marble/ImGui/ImGuiLayer.java
+++ src/main/java/marble/ImGui/ImGuiLayer.java
@@ -1,14 +1,26 @@
 package marble.imgui;
 
 import imgui.ImGui;
+import imgui.type.ImBoolean;
+
+import static org.lwjgl.glfw.GLFW.*;
 
 
 public class ImGuiLayer {
+
+    private final ImBoolean vsync = new ImBoolean(true);
+
     public void createLayer(float dt)
     {
-        ImGui.begin("Editor");
-        ImGui.text(String.format("%.3f", dt * 1000) + " ms/frame");
+        ImGui.begin("Diagnostics");
         ImGui.text(String.format("%.1f", 1 / dt) + " fps");
+        ImGui.text(String.format("%.3f", dt * 1000) + " ms/frame");
+        if (ImGui.checkbox("Vsync", vsync)) {
+            if(vsync.get())
+                glfwSwapInterval(GLFW_TRUE);
+            else
+                glfwSwapInterval(GLFW_FALSE);
+        }
         ImGui.end();
     }
 }
