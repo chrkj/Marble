4b8944efce41732b3b19fda7ec0f68ef8ea23511
Christian Kjær <chrkj@itu.dk>
2022-01-17 00:44:49 +0100

    Refactor: Vertex buffer

--- src/main/java/marble/GameObject/Components/Mesh.java
+++ src/main/java/marble/GameObject/Components/Mesh.java
@@ -5,6 +5,7 @@ import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import marble.renderer.VertexBuffer;
 import org.lwjgl.system.MemoryUtil;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
@@ -22,66 +23,23 @@ public class Mesh extends Component {
     private final boolean enableSmoothShading;
     private final List<Integer> vbos = new ArrayList<>();
 
-    public Mesh(float[] verticesArray, float[] textureArray, int[] indicesArray, float[] normals, boolean enableSmoothShading)
+    public Mesh(float[] verticesArray, float[] textureArray, int[] indicesArray, float[] normalsArray, boolean enableSmoothShading)
     {
         this.vertexCount = indicesArray.length;
         this.enableSmoothShading = enableSmoothShading;
-        FloatBuffer positionBuffer = null;
-        FloatBuffer textCoordsBuffer = null;
-        FloatBuffer vecNormalsBuffer = null;
-        IntBuffer indiceBuffer = null;
-        try {
-            // Create VAO
-            vaoId = glGenVertexArrays();
-            glBindVertexArray(vaoId);
 
-            // Position VBO
-            int posVboId = glGenBuffers();
-            vbos.add(posVboId);
-            positionBuffer = MemoryUtil.memAllocFloat(verticesArray.length);
-            positionBuffer.put(verticesArray).flip();
-            glBindBuffer(GL_ARRAY_BUFFER, posVboId);
-            glBufferData(GL_ARRAY_BUFFER, positionBuffer, GL_STATIC_DRAW);
-            glEnableVertexAttribArray(0);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        // Create and bind VAO
+        vaoId = glGenVertexArrays();
+        glBindVertexArray(vaoId);
 
-            // Texture coordinates VBO
-            int textureVboId = glGenBuffers();
-            vbos.add(textureVboId);
-            textCoordsBuffer = MemoryUtil.memAllocFloat(textureArray.length);
-            textCoordsBuffer.put(textureArray).flip();
-            glBindBuffer(GL_ARRAY_BUFFER, textureVboId);
-            glBufferData(GL_ARRAY_BUFFER, textCoordsBuffer, GL_STATIC_DRAW);
-            glEnableVertexAttribArray(1);
-            glVertexAttribPointer(1, 2, GL_FLOAT, false, 0, 0);
+        // Create VBO's
+        vbos.add(VertexBuffer.createVbo(verticesArray, 0, 3));
+        vbos.add(VertexBuffer.createVbo(textureArray, 1, 2));
+        vbos.add(VertexBuffer.createVbo(normalsArray, 2, 3));
+        vbos.add(VertexBuffer.createIndexVbo(indicesArray));
 
-            // Vertex normals VBO
-            int normVboId = glGenBuffers();
-            vbos.add(normVboId);
-            vecNormalsBuffer = MemoryUtil.memAllocFloat(normals.length);
-            vecNormalsBuffer.put(normals).flip();
-            glBindBuffer(GL_ARRAY_BUFFER, normVboId);
-            glBufferData(GL_ARRAY_BUFFER, vecNormalsBuffer, GL_STATIC_DRAW);
-            glVertexAttribPointer(2, 3, GL_FLOAT, false, 0, 0);
-
-            // Index VBO
-            int idxVboId = glGenBuffers();
-            vbos.add(idxVboId);
-            indiceBuffer = MemoryUtil.memAllocInt(indicesArray.length);
-            indiceBuffer.put(indicesArray).flip();
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxVboId);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceBuffer, GL_STATIC_DRAW);
-
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindVertexArray(0);
-        } finally {
-            if (positionBuffer != null)
-                MemoryUtil.memFree(positionBuffer);
-            if (textCoordsBuffer != null)
-                MemoryUtil.memFree(textCoordsBuffer);
-            if (indiceBuffer != null)
-                MemoryUtil.memFree(indiceBuffer);
-        }
+        // Unbind VAO
+        glBindVertexArray(0);
     }
 
     @Override
---
+++ src/main/java/marble/Renderer/VertexBuffer.java
@@ -0,0 +1,40 @@
+package marble.renderer;
+
+import java.nio.IntBuffer;
+import java.nio.FloatBuffer;
+
+import org.lwjgl.system.MemoryUtil;
+
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+
+public class VertexBuffer {
+
+    public static int createVbo(float[] data, int index, int size)
+    {
+        int vboID = glGenBuffers();
+        FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
+        buffer.put(data).flip();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(index);
+        glVertexAttribPointer(index, size, GL_FLOAT, false, 0, 0);
+        MemoryUtil.memFree(buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        return vboID;
+    }
+
+    public static int createIndexVbo(int[] data)
+    {
+        int indexVboId = glGenBuffers();
+        IntBuffer buffer = MemoryUtil.memAllocInt(data.length);
+        buffer.put(data).flip();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexVboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, buffer, GL_STATIC_DRAW);
+        MemoryUtil.memFree(buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        return indexVboId;
+    }
+}
