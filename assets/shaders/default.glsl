b58d9b7f552885c5496d35f8d5ff3fc3663f6952
Christian Kjær <c-kj@mail.dk>
2022-11-05 21:42:22 +0100

    Clean up

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -172,18 +172,21 @@ float ShadowCalculation(vec4 fragPosLightSpace, vec3 lightPos)
 {
     // perform perspective divide
     vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+
     // transform to [0,1] range
     projCoords = projCoords * 0.5 + 0.5;
+
     // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
     float closestDepth = texture(shadowMap, projCoords.xy).r;
+
     // get depth of current fragment from light's perspective
     float currentDepth = projCoords.z;
+
     // calculate bias (based on depth map resolution and slope)
     vec3 normal = normalize(fVertexNormal);
     vec3 lightDir = normalize(lightPos - fFragPos);
     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
-    // check whether current frag pos is in shadow
-    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+
     // PCF
     float shadow = 0.0;
     vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
